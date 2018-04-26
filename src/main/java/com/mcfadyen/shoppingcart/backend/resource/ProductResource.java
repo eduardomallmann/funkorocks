457d4751510fee306e32e51bb6050dc17e2f1634
Eduardo Mallmann <mallmann.edu@gmail.com>
2018-04-26 00:38:52 -0300

    Add product resource and service completed

--- src/main/java/com/mcfadyen/shoppingcart/backend/resource/ProductResource.java
+++ src/main/java/com/mcfadyen/shoppingcart/backend/resource/ProductResource.java
@@ -10,22 +10,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Class responsible for the endpoints of the {@link Product} object.
+ * <p>
+ * <p>Uses the {@link ProductService} to access the business logic of the object</p>
+ *
+ * @author Eduardo Mallmann
+ * @since 0.0.1
+ */
 @RestController
 @RequestMapping("/products")
 public class ProductResource {
 
     private ProductService productService;
 
+    /**
+     * Main constructor responsible to inject the services.
+     *
+     * @param productService service of the product to be injected in the class
+     * @since 0.0.1
+     */
     public ProductResource(ProductService productService) {
         this.productService = productService;
     }
 
+    /**
+     * Method responsible for send a list with all products to the request owner in JSON by the calling of its endpoint.
+     *
+     * @return a response with the list of all the products
+     * @since 0.0.1
+     */
     @GetMapping
     public ResponseEntity<List<Product>> productsGet() {
         // get the all the products and instatiate them inside the list
         List<Product> products = productService.findAllProducts();
         // return the list and successful status
         return new ResponseEntity<>(products, HttpStatus.OK);
-
     }
 }
--- src/main/java/com/mcfadyen/shoppingcart/backend/service/ProductService.java
+++ src/main/java/com/mcfadyen/shoppingcart/backend/service/ProductService.java
@@ -1,7 +1,69 @@
 package com.mcfadyen.shoppingcart.backend.service;
 
+import com.mcfadyen.shoppingcart.backend.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Class responsible for the business logic of the {@link Product} object.
+ *
+ * @author Eduardo Mallmann
+ * @since 0.0.1
+ */
 @Service
 public class ProductService {
+
+    private static List<Product> products = new ArrayList<>();
+
+    /**
+     * Static block responsible to create and add the product objects in its list.
+     */
+    static {
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Joey Ramone - Ramones",
+                "https://images-na.ssl-images-amazon.com/images/I/41i3tE9rSlL.jpg", new BigDecimal(5.98)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Elton John",
+                "https://images-na.ssl-images-amazon.com/images/I/41-Cu54MYqL.jpg", new BigDecimal(10.30)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Jimi Hendrix",
+                "https://images-na.ssl-images-amazon.com/images/I/41Baiq1QkpL.jpg", new BigDecimal(8.10)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Lemmy Kilmister - Motörhead",
+                "https://images-na.ssl-images-amazon.com/images/I/51WpJ-s-D6L.jpg", new BigDecimal(9.94)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Kurt Cobain - Nirvana",
+                "https://images-na.ssl-images-amazon.com/images/I/41QG9-Ewk-L.jpg", new BigDecimal(10.00)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Axl Rose - Guns N’Roses",
+                "https://images-na.ssl-images-amazon.com/images/I/61z3DkOYbHL._SL1221_.jpg",
+                new BigDecimal(5.99)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Duff Mckagan - Guns N’Roses",
+                "https://images-na.ssl-images-amazon.com/images/I/51AUT%2BpucUL.jpg", new BigDecimal(9.98)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Slash - Guns N’Roses",
+                "https://images-na.ssl-images-amazon.com/images/I/41HZYtnAMfL.jpg", new BigDecimal(12.52)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Kirk Hammett - Metallica",
+                "https://images-na.ssl-images-amazon.com/images/I/51FRYtesEOL.jpg", new BigDecimal(8.25)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Lars Ulrich - Metallica",
+                "https://images-na.ssl-images-amazon.com/images/I/41KVf8XtVcL.jpg", new BigDecimal(7.66)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Robert Trujillo - Metallica",
+                "https://images-na.ssl-images-amazon.com/images/I/51iKUKv6igL.jpg", new BigDecimal(7.99)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "James Hetfield - Metallica",
+                "https://images-na.ssl-images-amazon.com/images/I/41MxOyKK9gL.jpg", new BigDecimal(6.63)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Vince Neil - Mötley Crüe",
+                "https://images-na.ssl-images-amazon.com/images/I/51t%2BrDpYpGL.jpg", new BigDecimal(10.99)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Nikki Six - Mötley Crüe",
+                "https://images-na.ssl-images-amazon.com/images/I/41ZfjnVvUyL.jpg", new BigDecimal(10.99)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Tommy Lee - Mötley Crüe",
+                "https://images-na.ssl-images-amazon.com/images/I/51vvWZSX7UL.jpg", new BigDecimal(10.99)));
+        products.add(new Product(String.valueOf(UUID.randomUUID()), "Mick Mars - Mötley Crüe",
+                "https://images-na.ssl-images-amazon.com/images/I/51cEt69h7AL.jpg", new BigDecimal(10.99)));
+    }
+
+    /**
+     * Method that finds all the objects of the product model.
+     *
+     * @return list with all the product objects
+     */
+    public List<Product> findAllProducts() {
+        return products;
+    }
 }
